8050bbd70c9d5347a07946ad93612dca6c94d115
Chris Nikolaidis <Chris@Life-t.gr>
2019-05-28 09:52:20 +0300

    testing commit

--- test/Campaing.test.js
+++ test/Campaing.test.js
@@ -4,17 +4,66 @@ const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
-const compileCampaing = require('../ethereum/build/Campaign.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
 
 let accounts;
 let factory;
-let campaingAddress;
-let campaing;
+let campaignAddress;
+let campaign;
+
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-     .deploy({ data: compiledFactory.bytecode })
-     .sent({ from: accounts[0], gas: '1000000' });
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+
+    await factory.methods.createCampaign('100').send({ 
+        from: accounts[0],
+        gas: '1000000'
+    });
+
+    //fist element and assing to campaignAddress
+    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+    campaign = await new web3.eth.Contract (
+        JSON.parse(compiledCampaign.interface),
+        campaignAddress
+    );
+});
+
+describe('Campaings', () => {
+    it('deploys a factory and a campaign', () => {
+        assert.ok(factory.options.address);
+        assert.ok(campaign.options.address);
+    });
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it('allows people to contribute money and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1] 
+        });
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+        assert(isContributor);
+    });
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '50',
+                from: accounts[1]
+            });
+            assert(false);
+        } catch (err) {
+            assert(err)
+        }
+        
+    });
+
+
 });
