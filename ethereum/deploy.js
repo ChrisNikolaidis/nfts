ebd20f5b2da353ecd1e5d377750541fc904136c2
Chris Nikolaidis <Chris@Life-t.gr>
2019-05-28 11:10:37 +0300

    work

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,8 +3,8 @@ const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
-  'game saddle oyster laundry equal loop lunch allow cactus endless hover unfair',
-  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+  'inject unveil earn various size comic ticket stamp little surprise hen alone',
+  'rinkeby.infura.io/v3/402014c5be4e43cab02965ca953e90a3'
 );
 const web3 = new Web3(provider);
 
@@ -14,7 +14,7 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-    .deploy({ data: bytecode })
+    .deploy({ data: compiledFactory.bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instace = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xAaae2f887543F03AAC81A3d62D4A7987c7e28521'
+    '0xb88c7568cd46456e329d63b4d2987d24be04fd0e'
 );
 
 export default instace;
